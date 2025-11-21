93f2749ae634e5e1e1d205d55fa920f40ad19597
imshubham024 <shubham24042003@gmail.com>
2025-11-22 00:37:15 +0530

    server seetup

---
+++ cmd/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/imshubham024/go-emailVerify/api"
+)
+
+func main() {
+	router := gin.Default()
+	//initialize the config
+	app := api.Config{Router: router}
+	app.Routes()
+	router.Run(":8082")
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module ggithub.com/imshubham024/emailVerify
 
 go 1.25.1
+
+require github.com/joho/godotenv v1.5.1
